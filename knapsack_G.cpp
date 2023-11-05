310ef588476b9ca0df2763a23bf6845141cb082a
ukadepoojasampat19 <115064113+ukadepoojasampat19@users.noreply.github.com>
2023-11-05 19:26:39 +0530

    fractional_knapsack.cpp

--- knapsack_G.cpp
+++ knapsack_G.cpp
@@ -1,145 +1,192 @@
-#include <iostream>
+// Neha Mundase
+// TIB47
+
+// Fractional KnapSack
+
+
+#include<iostream>
 using namespace std;
 
-// Greedy
-struct Object
-{
-    float pt;
-    float wt;
-    float ratio;
-    void rt()
-    {
-        ratio = pt / wt;
-    }
-};
+//pw1 contains original p/w ratio and pw2 contains sorted p/w order
 
-void bubblesort(Object s[], int n)
-{
-    for (int i = 1; i < n; i++)
-    {
-        for (int j = 0; j < n - i; j++)
-        {
-            if (s[j].pt > s[j + 1].pt)
-            {
-                Object temp = s[j];
-                s[j] = s[j + 1];
-                s[j + 1] = temp;
+//calculate the profit/weight ratio
+void calc_pw(float p[],float w[],float pw1[],float pw2[],int n){
+    for(int i=0;i<n;i++){
+        pw1[i]=pw2[i]=(p[i])/(w[i]);
+    }
+}
+
+//sort the p/w ratio in decreasing order
+void sort_pw(float pw2[],int n){
+    for(int i=0;i<n;i++){
+        for(int j=i+1;j<n;j++){
+            if(pw2[i]<pw2[j]){
+                float temp;
+                temp=pw2[i];
+                pw2[i]=pw2[j];
+                pw2[j]=temp;
             }
         }
     }
 }
 
-void sortByRatio(Object s[], int n)
-{
-    for (int i = 1; i < n; i++)
-    {
-        for (int j = 0; j < n - i; j++)
-        {
-            if (s[j].ratio > s[j + 1].ratio)
-            {
-                Object temp = s[j];
-                s[j] = s[j + 1];
-                s[j + 1] = temp;
+void display_pw(float pw[],int n){
+    for(int i=0;i<n;i++){
+        cout<<pw[i]<<"\t";
+    }
+}
+
+void input(float a[],int n){
+    for(int i=0;i<n;i++){
+        cout<<"\n"<<"Enter the values:"<<"\n";
+        cin>>a[i];
+    }
+}
+
+void knapSack(float p[],float w[],float pw1[], float pw2[],int n,int max_wt){
+    float net_profit= 0;
+    float remaining_wt=max_wt;
+    float sum_wt=0;
+    int trackArray[n];
+
+        for(int i=0;i<n;i++){
+            trackArray[i]=0;
+        }
+
+        while(sum_wt < max_wt){
+            for(int i=0;i<n;i++){
+            int temp;
+            for(int j=0;j<n;j++){
+                if(pw2[i]==pw1[j] && trackArray[j]==0){
+                    temp=j;
+                    if(w[temp]<=remaining_wt){
+                        trackArray[j]=1;
+                        cout<<w[temp]<<"\n";
+                        remaining_wt=remaining_wt-w[temp];
+                        sum_wt=sum_wt+w[temp]; 
+                        net_profit=net_profit+p[temp];
+                        break;
+                    }
+                    else{
+                        trackArray[j]=1;
+                        float wt = (remaining_wt/w[temp]);
+                        cout<<wt<<"\n";
+                        remaining_wt=remaining_wt-wt;
+                        sum_wt=sum_wt+wt;
+                        net_profit=net_profit + (wt*pw1[temp]);
+                        break;
+                }
             }
         }
     }
+    // // Break the loop if the sum_wt exceeds max_wt 
+        if ((sum_wt-max_wt)<=0.000001) {
+            break;
+        }
+}
+    //cout<<remaining_wt<<"\n";
+    cout<<"\n"<<"Net profit:"<<"\n";
+    cout<<net_profit;
 }
 
-float profit(Object s[], int n, float tot)
-{
-    float temp = tot;
-    float profit = 0;
-    for (int i = n - 1; i >= 0 && temp > 0; i--)
-    {
-        if (temp >= s[i].wt)
-        {
-            temp -= s[i].wt;
-            profit += s[i].pt;
-        }
-        else if (temp < s[i].wt)
-        {
-            profit += (temp * s[i].ratio);
-            return profit;
-        }
-    }
-    return profit;
-}
+int main(){
+    int n,max_wt=15;
+    cout<<"\n"<<"Enter the number of object:"<<"\n";
+    cin>>n;
+    float p[20];
+    float w[20];
+    float pw1[20];
+    float pw2[20];
 
+    cout<<"\n"<<"Enter the profit for each object:"<<"\n";
+    input(p,n);
+    cout<<"\n"<<"Enter the weight for each object:"<<"\n";
+    input(w,n);
 
+    calc_pw(p,w,pw1,pw2,n);
 
-int main()
-{
-    int num;
-    float tot;
-    cout << "enter number of objects\n";
-    cin >> num;
-    cout << "enter total wt\n";
-    cin >> tot;
-    Object obj[20];
+    sort_pw(pw2,n);
 
-    for (int i = 0; i < num; i++)
-    {
-        cout << "enter pr of obj\n";
-        cin >> obj[i].pt;
-        cout << "enter wt of obj\n";
-        cin >> obj[i].wt;
-        obj[i].rt();
-    }
-    //bubblesort(obj, num);
-    sortByRatio(obj , num);
+    cout<<"\n"<<"The p/w ratio before sorting :"<<"\n";
+    display_pw(pw1,n);
+    
+    cout<<"\n"<<"The p/w ratio after sorting :"<<"\n";
+    display_pw(pw2,n);
 
-    for (int i = 0; i < num; i++)
-    {
-        cout << i << " " << obj[i].pt << " " << obj[i].wt << " " << obj[i].ratio << endl;
-    }
-    cout << endl;
-    float pr = profit(obj, num, tot);
-    cout << "profit is " << pr << endl;
+    cout<<"\n"<<"Weights added into the bag:"<<"\n";
+    knapSack(p,w,pw1,pw2,n,max_wt);
 
     return 0;
 }
 
-/*
-enter number of objects
-7
-enter total wt
-16
-enter pr of obj
-10
-enter wt of obj
-2
-enter pr of obj
-15
-enter wt of obj
-4
-enter pr of obj
-12
-enter wt of obj
-5
-enter pr of obj
-4
-enter wt of obj
-4
-enter pr of obj
-6
-enter wt of obj
-2
-enter pr of obj
-16
-enter wt of obj
-3
-enter pr of obj
-8
-enter wt of obj
-3
-0 4 4 1
-1 12 5 2.4
-2 8 3 2.66667
-3 6 2 3
-4 15 4 3.75
-5 10 2 5
-6 16 3 5.33333
 
-profit is 59.8
-PS C:\Users\Admin\Desktop\TA67> 
-*/
+
+/* OUTPUT : 
+
+PS D:\DAA> g++ knap_sack.cpp
+PS D:\DAA> ./a.exe
+
+Enter the number of object:
+7
+
+Enter the profit for each object:
+
+Enter the values:
+10
+
+Enter the values:
+5
+
+Enter the values:
+15
+
+Enter the values:
+7
+
+Enter the values:
+6
+
+Enter the values:
+18
+
+Enter the values:
+3
+
+Enter the weight for each object:
+
+Enter the values:
+2
+
+Enter the values:
+3
+
+Enter the values:
+5
+
+Enter the values:
+7
+
+Enter the values:
+1
+
+Enter the values:
+4
+
+Enter the values:
+1
+
+The p/w ratio before sorting :
+5       1.66667 3       1       6       4.5     3
+The p/w ratio after sorting :
+6       5       4.5     3       3       1.66667 1
+Weights added into the bag:
+1
+2
+4
+5
+1
+0.666667
+0.190476
+
+Net profit:
+53.3016 */
