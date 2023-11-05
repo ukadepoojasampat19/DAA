ed078eb763d0dfe7c06eff4de09d8400fbfcb6c4
ukadepoojasampat19 <115064113+ukadepoojasampat19@users.noreply.github.com>
2023-11-05 19:32:27 +0530

    Update tsp.cpp

--- tsp.cpp
+++ tsp.cpp
@@ -1,134 +1,150 @@
+// Write a program to solve the travelling salesman problem and to print the path and the cost using LC Branch and Bound. 
+
 #include <iostream>
-#include <limits>
-#include <vector>
+#include <algorithm>
+#include <climits>
 using namespace std;
 
-int inf = numeric_limits<int>::max();
+const int MAX = 10;
 
-void display(vector<vector<int>> mat, int n)
-{
+int n;
+int graph[MAX][MAX];
+int finalPath[MAX]; 
+int finalCost = INT_MAX;
+
+void copyToFinalPath(int currentPath[]) {
     for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            cout << mat[i][j] << "\t";
+        finalPath[i] = currentPath[i];
+    finalPath[n] = currentPath[0];
+}
+
+int firstMin(int i) {
+    int min = INT_MAX;
+    for (int k = 0; k < n; k++)
+        if (graph[i][k] < min && i != k)
+            min = graph[i][k];
+    return min;
+}
+
+int secondMin(int i) {
+    int first = INT_MAX, second = INT_MAX;
+    for (int j = 0; j < n; j++) {
+        if (i == j)
+            continue;
+        if (graph[i][j] <= first) {
+            second = first;
+            first = graph[i][j];
+        } else if (graph[i][j] <= second && graph[i][j] != first) {
+            second = graph[i][j];
         }
-        cout << endl;
+    }
+    return second;
+}
+
+void TSPRec(int curr_bound, int curr_weight, int level, int currentPath[]) {
+    if (level == n) {
+        if (graph[currentPath[level - 1]][currentPath[0]] != 0) {
+            int curr_cost = curr_weight + graph[currentPath[level - 1]][currentPath[0]];
+            if (curr_cost < finalCost) {
+                copyToFinalPath(currentPath);
+                finalCost = curr_cost;
+            }
+        }
+        return;
+    }
+
+    for (int i = 0; i < n; i++) {
+        if (graph[currentPath[level - 1]][i] != 0 && find(currentPath, currentPath + level, i) == currentPath + level) {
+            int temp = curr_bound;
+            curr_weight += graph[currentPath[level - 1]][i];
+
+            if (level == 1)
+                curr_bound -= (firstMin(currentPath[level - 1]) + firstMin(i)) / 2;
+            else
+                curr_bound -= (secondMin(currentPath[level - 1]) + firstMin(i)) / 2;
+
+            if (curr_bound + curr_weight < finalCost) {
+                currentPath[level] = i;
+                TSPRec(curr_bound, curr_weight, level + 1, currentPath);
+            }
+
+            curr_weight -= graph[currentPath[level - 1]][i];
+            curr_bound = temp;
+
+            fill(currentPath + level, currentPath + n, -1);
+        }
     }
 }
 
-int red_row(vector<vector<int>> &mat, int n)
-{
-    int *mini = new int[n];
-    int sum = 0;
+bool visited[MAX];
+
+void TSP() {
+    int currentPath[MAX];
+    int curr_bound = 0;
+    fill(currentPath, currentPath + MAX, -1);
+    fill(visited, visited + MAX, false);
 
     for (int i = 0; i < n; i++)
-    {
-        int minn = 999;
-        for (int j = 0; j < n; j++)
-        {
-            int ele = mat[i][j];
-            if (mat[i][j] == 999)
-            {
-                continue;
-            }
-            minn = min(minn, mat[i][j]);
-        }
-        if (minn == 999)
-        {
-            minn = 0;
-        }
-        mini[i] = minn;
-        for (int j = 0; j < n; j++)
-        {
-            int ele = mat[i][j];
-            if (mat[i][j] == 999)
-            {
-                continue;
-            }
-            mat[i][j] -= mini[i];
-        }
-        sum += mini[i];
-    }
-    return sum;
+        curr_bound += (firstMin(i) + secondMin(i));
+
+    curr_bound = (curr_bound & 1) ? curr_bound / 2 + 1 : curr_bound / 2;
+
+    currentPath[0] = 0;
+    visited[0] = true;
+    TSPRec(curr_bound, 0, 1, currentPath);
 }
 
-int red_col(vector<vector<int>> &mat, int n)
-{
-    int row = red_row(mat, n);
-    int mini[10];
-    int col = 0;
 
+int main() {
+    cout << "Enter the number of cities: ";
+    cin >> n;
+
+    cout << "Enter the adjacency matrix representing the graph:" << endl;
     for (int i = 0; i < n; i++)
-    {
-        int minn = 999;
         for (int j = 0; j < n; j++)
-        {
+            cin >> graph[i][j];
 
-            if (mat[j][i] == 999)
-            {
-                continue;
-            }
-            minn = min(minn, mat[j][i]);
-        }
-        if (minn == 999)
-        {
-            minn = 0;
-        }
-        mini[i] = minn;
-        for (int j = 0; j < n; j++)
-        {
+    TSP();
 
-            if (mat[j][i] == 999)
-            {
-                continue;
-            }
-            mat[j][i] -= mini[i];
-        }
-        col += mini[i];
-    }
-    return col + row;
-}
+    cout << "Optimal Path: ";
+    for (int i = 0; i <= n; i++)
+        cout << finalPath[i] << " ";
+    cout << endl;
 
-void mk_inf(vector<vector<int>> &mat, int n, int x, int y)
-{
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            if (i == x || j == y || (i == y && j == x))
-            {
-                mat[i][j] = 999;
-            }
-        }
-    }
-}
-
-int tsp(vector<vector<int>> mat, int n, int x, int y, vector<bool> vis)
-{
-    vector<vector<int>> copy = mat;
-    vector<bool> visited = vis;
-    if (!visited[x])
-    {
-        visited[x] = true;
-        mk_inf(copy, n, x, y);
-        display(copy, n);
-         red_col(copy, n);
-    }
-}
-
-int main()
-{
-    int n = 5;
-    vector<vector<int>> matrix = {{999, 20, 30, 10, 11},
-                                  {15, 999, 16, 4, 2},
-                                  {3, 5, 999, 2, 4},
-                                  {19, 6, 18, 999, 3},
-                                  {16, 4, 7, 16, 999}};
-    cout << red_col(matrix, n) << endl;
-    // display(matrix, n);
-
-    /* code */
+    cout << "Minimum Cost: " << finalCost << endl;
 
     return 0;
 }
+
+// OUTPUT >>
+
+// Enter the number of cities: 5
+// Enter the adjacency matrix representing the graph:
+// 0
+// 20
+// 30
+// 10
+// 11
+// 15
+// 0
+// 16
+// 14
+// 2
+// 3
+// 5
+// 0
+// 2
+// 4
+// 19
+// 6
+// 18
+// 0
+// 3
+// 16
+// 4
+// 7
+// 16
+// 0
+// Optimal Path: 0 3 1 4 2 0
+// Minimum Cost: 28
+// PS P:\VsCode>
